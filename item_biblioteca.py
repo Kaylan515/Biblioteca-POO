e8e3d9b9b53bb45aff324d90d5c17ce92dec21df
Kaylan <kaylanteodoro344@gmail.com>
2026-03-02 10:53:41 -0300

    Criando a primeira classe ✔

---
+++ item_biblioteca.py
@@ -0,0 +1,7 @@
+class Itembibliteca():
+    def __init__(self, codigo, titulo, ano, disponivel):
+        self.__codigo = codigo
+        self.titulo = titulo
+        self.ano = ano
+        self.ano = disponivel
+    
