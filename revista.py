a1b9723e84c709e1d298d175d5ef3eb5830e41da
Kaylan <kaylanteodoro344@gmail.com>
2026-03-03 09:34:54 -0300

    Corrigindo  Revista ⚙

--- revista.py
+++ revista.py
@@ -1,11 +1,11 @@
 from item_biblioteca import Itembiblioteca
 
 class Revista(Itembiblioteca):
-    def __init__(self, codigo, titulo, ano: int, disponivel: bool, editora: str, volume: int):
+    def __init__(self, codigo, titulo, ano: int, disponivel: bool, editora: str, mes: int):
         super().__init__(codigo, titulo, ano, disponivel)
         self.__editora = editora
-        self.__volume = volume
+        self.__mes = mes
 
     def exibir_detalhes(self):
         super().exibir_detalhes()
-        print(f"Editora da revista: {self.editora}, volume: {self.volume}")
+        print(f"Editora da revista: {self.editora}, mês: {self.mes}")
